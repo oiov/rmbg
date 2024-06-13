024f1a5f753540dbcc54df8bd628d520708ab0f7
oiov <stacystayfine718@gmail.com>
2024-06-13 15:14:29 +0800

    fixup

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <head />
         <body
           className={cn(
-            "min-h-screen font-sans antialiased h-full w-full bg-black bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]",
+            "min-h-screen font-sans antialiased size-full bg-black bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]",
             fontSans.variable
           )}
         >
